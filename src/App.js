883f7146fda3c54c742bcd097f9bca5eb9dd9d80
ayoub <ayoub.belghar@gmail.com>
2019-06-17 21:12:51 +0000

    add travis config file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          all is working fine
         </a>
       </header>
     </div>
